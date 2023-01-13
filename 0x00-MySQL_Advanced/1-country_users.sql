8446506e81a0123a5390708c54ab4875d1a5725f
Joelayo <joetech.ooj@gmail.com>
2023-01-13 01:11:58 +0100

    Project Files

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,7 @@
--- Creates a table with unique users.;
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
+-- Creates a table with unique users.
+CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255) NOT NULL,
-  country VARCHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );

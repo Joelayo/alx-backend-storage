970aaff3df13cb736f3b4a1975c7d08b0356d80f
Joelayo <joetech.ooj@gmail.com>
2023-01-15 02:18:05 +0100

    Task 4 Complete

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,13 +1,13 @@
 -- Creates a trigger that decreases the quantity
 -- of an item after adding a new order.
+DROP TRIGGER IF EXISTS reduce_quantity;
 DELIMITER $$
-
-CREATE TRIGGER decrease_item_quantity
+CREATE TRIGGER reduce_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-    UPDATE items SET quantity = quantity - NEW.quantity
-    WHERE item_id = NEW.item_id;
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
 END $$
-
 DELIMITER ;

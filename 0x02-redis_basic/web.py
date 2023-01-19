de6e17ae3d4c1d616d1b515c8cf87fbd4aef106f
Joelayo <joetech.ooj@gmail.com>
2023-01-19 04:17:39 +0100

    Adv Tasks

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python3
+'''A module with tools for request caching and tracking.
+'''
+import redis
 import requests
 from functools import wraps
-from cachetools import cached, TTLCache
+from typing import Callable
 
-# Create a cache with a maxsize of 100 and a ttl of 10 seconds
-cache = TTLCache(maxsize=100, ttl=10)
 
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
 def get_page(url: str) -> str:
-    # Use the cached decorator to cache the results with the key "count:{url}"
-    @cached(cache, key=f'count:{url}')
-    def _get_page():
-        # Use the requests module to obtain the HTML content of the URL
-        response = requests.get(url)
-        return response.text
-
-    return _get_page()
-
-# Bonus: Implement this use case with decorators
-def cache_page(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        url = args[0]
-        # Use the cached decorator to cache the results with the key "count:{url}"
-        @cached(cache, key=f'count:{url}')
-        def _get_page():
-            # Call the original function
-            return func(*args, **kwargs)
-
-        return _get_page()
-    return wrapper
-
-@cache_page
-def get_page_decorator(url: str) -> str:
-    # Use the requests module to obtain the HTML content of the URL
-    response = requests.get(url)
-    return response.text
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
